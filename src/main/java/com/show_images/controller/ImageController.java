82848a6915e129be1ce5476126a11c810c3a2f66
cgcontrerasm <cgcontrerasm@unal.edu.co>
2019-05-18 22:23:43 -0500

    Update ImageController.java

--- src/main/java/com/show_images/controller/ImageController.java
+++ src/main/java/com/show_images/controller/ImageController.java
@@ -5,8 +5,6 @@ import com.show_images.repository.ImageRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
@@ -21,21 +19,15 @@ public class ImageController {
 	@GetMapping
     public final String hola() throws UnknownHostException {
         return "Hola! Puedes encontrarme en " + InetAddress.getLocalHost().getHostAddress();
-    }
+	}
+	
+	@GetMapping("/images")
+    public List<Image> getAllImagesPublic() {
+        return imageRepository.findByPrivacy("1"); 	   		
+	}
 	
     @GetMapping("/images/{id_user}")
-    public List<Image> getAllImagesPublic(@PathVariable(value = "id_user") String id_user, @RequestHeader("token") String token) {
-    	if(token == null) {
-    		return imageRepository.findByPrivacyAndIduser("0", id_user);
-    	}else {
-    		RestTemplate restTemplate = new RestTemplate();
-    		String petition = "192.168.0.14:8080/" + token;
-    		String id_user_token = restTemplate.getForObject(petition, String.class);
-    		if (id_user == id_user_token) {
-                return imageRepository.findByPrivacyAndIduser("1", id_user);
-        	}else {
-                return imageRepository.findByPrivacyAndIduser("0", id_user);
-        	}
-    	} 	   		
+    public List<Image> getAllImagesPublic(@PathVariable(value = "id_user") String id_user) {
+        return imageRepository.findByPrivacyAndIduser("0", id_user); 	   		
     }
 }
